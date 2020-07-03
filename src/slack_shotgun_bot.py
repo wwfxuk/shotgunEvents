4040f0bbb3dd385aa0429f34659e4e8a57585626
Joseph Yu <josephyu2712+github@gmail.com>
2020-07-03 16:20:47 +0100

    Initial KnownUsers, no refresh yet

--- src/slack_shotgun_bot.py
+++ src/slack_shotgun_bot.py
@@ -1,6 +1,9 @@
+import collections
 import os
+import typing
 
 # import shotgun_api3
+
 from slack import WebClient
 
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
@@ -8,6 +11,87 @@ SLACK_USER_TOKEN = os.environ["SLACK_USER_TOKEN"]
 SLACK_BOT_APP_ID = os.environ["SLACK_BOT_APP_ID"]
 SC_BOT = WebClient(token=SLACK_BOT_TOKEN)
 SC_USER = WebClient(token=SLACK_USER_TOKEN)
+
+SlackSgID = collections.namedtuple("SlackSgID", ["slack", "sg"])
+
+
+class KnownUsers(collections.abc.MutableSet):
+    def __init__(self):
+        self._ids_set = set()
+
+    def __contains__(self, *args, **kwargs):
+        return self._ids_set.__contains__(*args, **kwargs)
+
+    def __iter__(self, *args, **kwargs):
+        return self._ids_set.__iter__(*args, **kwargs)
+
+    def __len__(self, *args, **kwargs):
+        return self._ids_set.__len__(*args, **kwargs)
+
+    def refresh(self, sg):
+        """Clear and re-assign all Slack and Shotgun ID mappings."""
+
+    def sg_of(self, slack_id: str, default=None) -> int:
+        """Get Shotgun HumanUser ID for a given Slack ID."""
+        finder = (sg_id for id_, sg_id in self._ids_set if id_ == slack_id)
+        return next(finder, default)
+
+    def slack_of(self, sg_id: int, default=None) -> str:
+        """Get Slack ID for a given Shotgun HumanUser ID."""
+        finder = (slack_id for slack_id, id_ in self._ids_set if id_ == sg_id)
+        return next(finder, default)
+
+    @typing.overload
+    def discard(self, slack_id: str):
+        """Add a given Slack and Shotgun ID separately."""
+
+    @typing.overload
+    def discard(self, sg_id: int):
+        """Add a given Slack and Shotgun ID separately."""
+
+    @typing.overload
+    def discard(self, id_mapping: SlackSgID):
+        """Add a Slack and Shotgun ID mapping."""
+
+    def discard(self, id_):
+        if isinstance(id_, int):
+            id_mapping = SlackSgID(self.slack_of(id_), id_)
+        elif isinstance(id_, str):
+            id_mapping = SlackSgID(id_, self.sg_of(id_))
+        elif isisntance(id_, SlackSgID):
+            id_mapping = id_
+        else:
+            raise TypeError(f"Invalid ID to discard: {id_}")
+        self._ids_set.discard(id_mapping)
+
+    @typing.overload
+    def add(self, slack_id: str, sg_id: int) -> SlackSgID:
+        """Add a given Slack and Shotgun ID separately."""
+
+    @typing.overload
+    def add(self, id_mapping: SlackSgID) -> SlackSgID:
+        """Add a Slack and Shotgun ID mapping."""
+
+    def add(self, *args):
+        args_count = len(args)
+        if args_count == 2:
+            id_mapping = SlackSgID(*args)
+        elif args_count == 1:
+            id_mapping = args[0]
+        else:
+            raise TypeError(
+                f"Expected either 1 SlackSgID argument or 2 arguments: "
+                f"Slack ID and Shotgun ID respectively. Got {args}"
+            )
+
+        if not isinstance(id_mapping, SlackSgID):
+            type_ = type(id_mapping).__name__
+            raise TypeError(f"id_mapping should be of type SlackSgID, not {type_}")
+        self._ids_set.add(id_mapping)
+        return id_mapping
+
+
+KNOWN_USERS = KnownUsers()
 
 
 def send_message(channel, message, attachments=None):
